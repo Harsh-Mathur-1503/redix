afa09567db2941b0666c142dfdaebf3256132559
Harsh Mathur <harsh@Harshs-MacBook-Air.local>
2026-07-09 20:48:04 +0530

    build: add CMake skeleton alongwith clang-format and clang-tidy config

---
+++ build/CMakeFiles/redix.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/harsh/Developer/redix/bin/redix"
+  "/Users/harsh/Developer/redix/bin/redix.pdb"
+  "CMakeFiles/redix.dir/src/main.cpp.o"
+  "CMakeFiles/redix.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/redix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/redix.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for redix.
+# This may be replaced when dependencies are built.
